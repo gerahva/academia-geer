9a6a9ac5ba7151f48c3b68756976542c815d3a40
Juan Carlos Campos <rapidclimate@gmail.com>
2018-10-22 12:57:40 -0500

    noooooooooooooooo

--- src/app/material-component/perfil-alumno/perfil-alumno.component.ts
+++ src/app/material-component/perfil-alumno/perfil-alumno.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Estatus } from "../../modelo/estatus";
+import { Globales } from "../../modelo/globales";
+import swal from "sweetalert2";
+
+import { HttpClient } from "@angular/common/http";
+
+import { Alumno } from "../../modelo/alumno";
+import { Profesor } from "../../modelo/profesor";
 
 @Component({
   selector: 'app-perfil-alumno',
@@ -7,9 +15,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilAlumnoComponent implements OnInit {
 
-  constructor() { }
+  claveAlumno:string
+  email:string
+  grupo:number
+  password:string
+  estaDesactivado=true;
+
+estatus:Estatus={}
+alumno:Alumno={};
+
+constructor(public http:HttpClient) { }
 
   ngOnInit() {
   }
+  buscarAlumno(){
 
+    console.log("La clave es  "+this.claveAlumno);
+    //La siguiente URL busca por clave del alumno
+    this.http
+    .get<Alumno>(Globales.urlBase + "/alumno-clave/"+this.claveAlumno)
+    .subscribe(respuesta => (this.alumno = respuesta));
+  setTimeout(() => {
+    console.log("Mensaje del servidor" + JSON.stringify(this.alumno));
+    this.estaDesactivado=false;
+  this.email=this.alumno.email;
+    this.grupo=this.alumno.grupo;
+    this.password=this.alumno.password;
+    }, 1500);
+  }
+
+  actualizarAlumno(){
+    this.alumno.email=this.email;
+    this.alumno.grupo=this.grupo;
+    this.alumno.password=this.password;
+   // console.log("Alumno a actualizar: "+JSON.stringify(this.alumno));
+
+
+    this.http
+    .put<Estatus>(Globales.urlBase + "/alumno-clave",this.alumno )
+    .subscribe(respuesta => (this.estatus = respuesta));
+  setTimeout(() => {
+    console.log("Mensaje del servidor" + JSON.stringify(this.estatus.mensaje));
+    this.estaDesactivado=false;
+  this.email=this.alumno.email;
+    this.grupo=this.alumno.grupo;
+    this.password=this.alumno.password;
+
+    swal(
+      "Alumno modificado",
+      "Perfil del alumno atualizado" ,
+      "success"
+    );
+  
+    }, 1500);
+  }
+  
 }
